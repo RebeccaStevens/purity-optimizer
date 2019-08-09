c75fb1f7be936ccd4c81b2a8c2c7b1ac63369bdb
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-08-10 02:22:12 +1200

    build: add rollup

---
+++ rollup.config.ts
@@ -0,0 +1,65 @@
+/**
+ * Rollup Config.
+ */
+
+import { basename } from 'path';
+import rollupPluginCommonjs from 'rollup-plugin-commonjs';
+import rollupPluginNodeResolve from 'rollup-plugin-node-resolve';
+import rollupPluginTypescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json';
+
+const common = {
+  input: 'src/index.ts',
+
+  external: (id) =>
+    // Not a Local File?
+    !(id.startsWith('.') || id.startsWith('/')),
+
+  treeshake: {
+    annotations: true,
+    propertyReadSideEffects: false
+  }
+};
+
+const cjs = {
+  ...common,
+
+  output: {
+    dir: './lib',
+    entryFileNames: basename(pkg.main),
+    chunkFileNames: 'common/[hash].js',
+    format: 'cjs',
+    sourcemap: false
+  },
+
+  plugins: [
+    rollupPluginNodeResolve(),
+    rollupPluginCommonjs(),
+    rollupPluginTypescript({
+      tsconfigOverride: { compilerOptions: { target: 'es5' } }
+    })
+  ]
+};
+
+const esm = {
+  ...common,
+
+  output: {
+    dir: './lib',
+    entryFileNames: basename(pkg.module),
+    chunkFileNames: 'common/[hash].mjs',
+    format: 'esm',
+    sourcemap: false
+  },
+
+  plugins: [
+    rollupPluginNodeResolve(),
+    rollupPluginCommonjs(),
+    rollupPluginTypescript({
+      tsconfigOverride: { compilerOptions: { target: 'es2017' } }
+    })
+  ]
+};
+
+export default [cjs, esm];
